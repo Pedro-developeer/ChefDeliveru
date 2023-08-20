117351a8a5e3c25efd849f8cc26fc2f0aa9f4f02
Pedro Henrique <pedro.brito@sankhya.com.br>
2023-08-20 12:54:03 -0300

    Initial Commit

---
+++ ChefDeliveru/ChefDeliveruApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveruApp.swift
+//  ChefDeliveru
+//
+//  Created by Pedro Henrique Santos on 20/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliveruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
